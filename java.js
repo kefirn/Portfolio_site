74e6e8ce3f4013d2e77a48d37cc3b587471de9e3
Ivan <stepanenkoiv87@mail.ru>
2022-02-19 16:59:30 +0300

    Add comment

--- java.js
+++ java.js
@@ -1,15 +1,21 @@
+//This code run after click on card. 
+//After click it will be add class for section "is-flipped"
 
+//This is code for card1
     var card1 = document.querySelector('.card1');
  card1.addEventListener( 'click', function() {
  
      card1.classList.toggle('is-flipped');
    });
+
+//This is code for card2
     var card2 = document.querySelector('.card2');
 card2.addEventListener( 'click', function() {
 
     card2.classList.toggle('is-flipped');
   });
 
+  //This is code for card3
   var card3 = document.querySelector('.card3');
   card3.addEventListener( 'click', function() {
   
